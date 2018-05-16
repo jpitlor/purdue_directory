37e803bb9948ef5ef6e320eb2d54480b4ee22222
Jordan Pitlor <jordan@jordanpitlor.com>
2018-05-16 00:05:33 -0400

    almost an MVP! just a couple loose UI ends

--- lib/widgets/person.dart
+++ lib/widgets/person.dart
@@ -1,69 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../directory_api/person.dart';
 
 class PersonView extends StatelessWidget {
-	final Person _person;
+  final Person _person;
 
-	PersonView(this._person);
+  PersonView(this._person);
 
-	void _call() {
+  Widget _buildColumn(IconData icon, String label, String value) {
+    if (value.substring(value.indexOf(":") + 1) == "") return null;
 
-	}
+    return new Column(
+      children: <Widget>[
+        new IconButton(icon: new Icon(icon), onPressed: () { launch(value); }),
+        new Text(label)
+      ],
+    );
+  }
 
-	void _text() {
+  Widget _buildActions() {
+    List<Widget> children = [
+      _buildColumn(FontAwesomeIcons.phone, "Call", "tel:" + _person.otherPhone),
+      _buildColumn(FontAwesomeIcons.envelope, "Text", "sms:" + _person.otherPhone),
+      _buildColumn(FontAwesomeIcons.at, "Email", "mailto:" + _person.email),
+      _buildColumn(FontAwesomeIcons.link, "URL", _person.url)
+    ];
+    children.removeWhere((w) => w == null);
 
-	}
+    return new Padding(
+        padding: new EdgeInsets.symmetric(vertical: 15.0),
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: children,
+        ));
+  }
 
-	void _email() {
+  Widget _buildRow(IconData iconData, String label, String property) {
+    if (property == "") return null;
 
-	}
+    Icon icon = iconData == null
+        ? new Icon(FontAwesomeIcons.chevronRight, color: Colors.white)
+        : new Icon(iconData);
 
-	void _goToWebsite() {
+    return new Padding(
+      padding: new EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+      child: new Row(children: [
+        new Padding(
+          padding: new EdgeInsets.only(right: 15.0),
+          child: icon,
+        ),
+        new Padding(
+            padding: new EdgeInsets.only(right: 10.0),
+            child:
+                new Text(label, style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0))
+        ),
+        new Text(
+          property,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 5,
+          style: new TextStyle(fontSize: 18.0),
+        )
+      ]),
+    );
+  }
 
-	}
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> children = [
+      _buildActions(),
+      _buildRow(FontAwesomeIcons.userCircle, "Name", _person.toString()),
+      _buildRow(null, "Alias", _person.alias),
+      _buildRow(null, "Nickname", _person.nickname),
+      _buildRow(null, "Qualified Name", _person.qualifiedName),
+      _buildRow(FontAwesomeIcons.envelope, "Email", _person.email),
+      _buildRow(FontAwesomeIcons.flag, "Campus", _person.campus),
+      _buildRow(FontAwesomeIcons.sitemap, "Department", _person.department),
+      _buildRow(null, "Title", _person.title),
+      _buildRow(FontAwesomeIcons.building, "School", _person.school),
+      _buildRow(FontAwesomeIcons.fax, "Fax", _person.fax),
+      _buildRow(FontAwesomeIcons.phone, "Phone", _person.otherPhone),
+      _buildRow(null, "Pager", _person.pager),
+      _buildRow(FontAwesomeIcons.building, "Building", _person.building),
+      _buildRow(null, "Office", _person.office),
+      _buildRow(FontAwesomeIcons.clock, "Office Hours", _person.officeHours),
+      _buildRow(FontAwesomeIcons.comment, "Comment", _person.comment),
+      _buildRow(null, "Project", _person.project),
+      _buildRow(FontAwesomeIcons.link, "URL", _person.url),
+      // todopgp key
+    ];
+    children.removeWhere((w) => w == null);
 
-	Widget _buildActions() {
-		return new Row(
-			mainAxisAlignment: MainAxisAlignment.spaceAround,
-			children: <Widget>[
-				new IconButton(icon: new Icon(Icons.phone), onPressed: _call),
-				new IconButton(icon: new Icon(Icons.message), onPressed: _text),
-				new IconButton(icon: new Icon(Icons.email), onPressed: _email),
-				new IconButton(icon: new Icon(Icons.link), onPressed: _goToWebsite)
-			],
-		);
-	}
-
-	@override
-	Widget build(BuildContext context) {
-		return new Scaffold(
-			appBar: new AppBar(
-				title: new Text(_person.toString())
-			),
-			body: new Column(
-				children: <Widget>[
-					_buildActions(),
-					// name
-					// alias
-					// nickname
-					// email
-					// campus
-					// dept
-					// title
-					// school
-					// fax
-					// pager
-					// phone
-					// building
-					// office
-					// office hours
-					// qualified name
-					// comment
-					// project
-					// url
-					// todo: pgp key
-				],
-			),
-		);
-	}
+    return new Scaffold(
+      appBar: new AppBar(title: new Text(_person.toString())),
+      body: new ListView(
+        children: children,
+      ),
+    );
+  }
 }
